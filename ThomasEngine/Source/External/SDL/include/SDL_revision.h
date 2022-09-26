c30e323d5b42ff211deccd8fbdd3050cdb5a63c6
enricmc19 <hnrtbn2001@gmail.com>
2022-09-26 08:27:48 +0200

    SDL actualizada

--- ThomasEngine/Source/External/SDL/include/SDL_revision.h
+++ ThomasEngine/Source/External/SDL/include/SDL_revision.h
@@ -1,2 +1,6 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@25f9ed87ff6947d9576fc9d79dee0784e638ac58"
+/* #undef SDL_REVISION */
 #define SDL_REVISION_NUMBER 0
+
+#ifndef SDL_REVISION
+#define SDL_REVISION ""
+#endif
