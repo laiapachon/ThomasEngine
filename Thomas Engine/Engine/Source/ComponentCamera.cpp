d1cc7285f328094c33920a85f44513a5d69ba3f7
enricmc19 <61042957+enricmc19@users.noreply.github.com>
2022-11-27 12:42:57 +0100

    Merge confusion solved

--- Thomas Engine/Engine/Source/ComponentCamera.cpp
+++ Thomas Engine/Engine/Source/ComponentCamera.cpp
@@ -1,10 +1,28 @@
 #include "Application.h"
 #include "ComponentCamera.h"
-#include "Transform.h"
+
 #include "Globals.h"
 #include "Scene.h"
-#include"Camera3D.h"
+#include "Camera3D.h"
+
+#include "Transform.h"
+
 #include "ImGui/imgui.h"
+
+
+ComponentCamera::ComponentCamera() : Component(nullptr)
+{
+	frustrum.type = FrustumType::PerspectiveFrustum;
+	frustrum.nearPlaneDistance = 0.1f;
+	frustrum.farPlaneDistance = 500.f;
+	frustrum.front = float3::unitZ;
+	frustrum.up = float3::unitY;
+
+	frustrum.verticalFov = 60.0f * DEGTORAD;
+	frustrum.horizontalFov = 2.0f * atanf(tanf(frustrum.verticalFov / 2.0f) * 1.3f);
+
+	frustrum.pos = float3::zero;
+}
 
 ComponentCamera::ComponentCamera(GameObject* obj) : Component(obj)
 {
@@ -15,17 +33,26 @@ ComponentCamera::ComponentCamera(GameObject* obj) : Component(obj)
 	frustrum.up = obj->transform->GetUp();
 
 	frustrum.verticalFov = 60.0f * DEGTORAD;
-	frustrum.horizontalFov = 2.0f * atanf(tanf(frustrum.verticalFov / 2.0f) * (16.f / 9.f));
+	frustrum.horizontalFov = 2.0f * atanf(tanf(frustrum.verticalFov / 2.0f) * 1.3f);
 
 	frustrum.pos = obj->transform->GetPosition();
+}
+
+ComponentCamera::~ComponentCamera()
+{
+	glDeleteFramebuffers(1, &framebuffer);
+	glDeleteTextures(1, &texColorBuffer);
+	glDeleteRenderbuffers(1, &rbo);
 }
 
 void ComponentCamera::Update()
 {
 	if (updateCamera)
 	{
-		CalculateViewMatrix();
-	}	
+		frustrum.pos = GetOwner()->transform->GetPosition();
+		frustrum.front = GetOwner()->transform->GetForward();
+		frustrum.up = GetOwner()->transform->GetUp();
+	}
 
 	if (showFrustrum == true) {
 		float3 points[8];
@@ -68,24 +95,60 @@ void ComponentCamera::OnEditor()
 	}
 }
 
-void ComponentCamera::CalculateViewMatrix()
+void ComponentCamera::PreUpdate()
 {
-	updateCamera = false;
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 
-	frustrum.pos = GetOwner()->transform->GetPosition();
-	frustrum.front = frustrum.front.Normalized();
-	frustrum.up = frustrum.up.Normalized();
-	
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
 
-	app->camera->viewMatrix = frustrum.ViewMatrix();
+	glLoadIdentity();
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf(frustrum.ProjectionMatrix().Transposed().ptr());
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixf(viewMatrix.Transposed().ptr());
 }
 
-void ComponentCamera::RecalculateProjection()
+void ComponentCamera::ReGenerateFrameBuffer(int w, int h)
 {
-	/*frustrum.type = FrustumType::PerspectiveFrustum;
+	if (framebuffer > 0)
+		glDeleteFramebuffers(1, &framebuffer);
 
-	frustrum.nearPlaneDistance = nearPlaneDistance;
-	frustrum.farPlaneDistance = farPlaneDistance;
-	frustrum.verticalFov = (verticalFOV * PI / 2) / 180.f;
-	frustrum.horizontalFov = 2.f * atanf(tanf(frustrum.verticalFov * 0.5f) * aspectRatio);*/
+	if (texColorBuffer > 0)
+		glDeleteTextures(1, &texColorBuffer);
+
+	if (rbo > 0)
+		glDeleteRenderbuffers(1, &rbo);
+
+
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+	glGenTextures(1, &texColorBuffer);
+	glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// attach it to currently bound framebuffer object
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
+
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, w, h);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		LOG(LogType::L_ERROR, "ERROR::FRAMEBUFFER:: Framebuffer is not complete!");
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void ComponentCamera::PostUpdate()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
